e9bf49bf24bd1162e9e556d0b99e69ceeef2c1e0
ahmad1596 <104497866+ahmad1596@users.noreply.github.com>
2023-09-06 11:47:04 +0100

    SuperKcompact.py

--- SuperKcompact.py
+++ SuperKcompact.py
@@ -1,45 +1,113 @@
-# Author: Ahmad Azizan (aaaba2@cam.ac.uk)
 import serial
-import time
 
-class SuperKCompactController:
-    def __init__(self, port, baudrate=115200):
-        self.serial_port = serial.Serial(port, baudrate, timeout=1)
-        self.addr_laser = '01'
-        self.msg_write = '01'
+class SuperKCompactControl:
+    def __init__(self, module_type='74', serial_port=None):
+        self.module_type = module_type
+        self.serial_port = serial_port
+        self.timeout = 0.05
 
-    def send_telegram(self, addr, msg_type, data):
-        telegram = f'{addr}{msg_type}{data}\r'
-        print("Sending telegram:", telegram)
-        self.serial_port.write(telegram.encode())
+    def connect(self):
+        if self.serial_port is None:
+            serial_port = 'COM3'
+            print(f"Attempting to open {serial_port}...")
+            try:
+                self.serial_port = serial.Serial(serial_port, baudrate=115200, timeout=self.timeout)
+                print(f"Successfully opened {serial_port}")
+                response = self.send_command(f'74{self.module_type}0000')
+                if response and response.startswith('74'):
+                    print(f'Successfully connected to COM port {serial_port}')
+                    print('SuperK Compact connected')
+                else:
+                    self.serial_port.close()
+                    self.serial_port = None
+            except Exception as e:
+                print(f'Error connecting to COM port {serial_port}: {str(e)}')
+    
+        if self.serial_port is None:
+            print('Failed to connect to any COM port.')
 
-    def get_response(self):
-        response = self.serial_port.read_until(b'\n').decode().strip()
-        print("Received response:", response)
-        return response
 
-    def turn_on(self):
-        data = '30' + '01' 
-        self.send_telegram(self.addr_laser, self.msg_write, data)
-        print("Laser turned ON")
-        self.get_response()  
+    def disconnect(self):
+        if self.serial_port:
+            self.serial_port.close()
+            self.serial_port = None
+            print('SuperK Compact disconnected')
 
-    def turn_off(self):
-        data = '30' + '00' 
-        self.send_telegram(self.addr_laser, self.msg_write, data)
-        print("Laser turned OFF")
-        self.get_response()  
+    def set_timeout(self, timeout):
+        self.timeout = timeout
 
-    def close(self):
-        self.serial_port.close()
+    def get_timeout(self):
+        return self.timeout
+
+    def send_command(self, command):
+        if self.serial_port:
+            self.serial_port.write(bytes.fromhex(command))
+            response = self.serial_port.read(128).hex()
+            return response
+        return None
+
+    def get_supply_voltage(self):
+        response = self.send_command('74001A0000')
+        if response and response.startswith('74001A'):
+            voltage_hex = response[6:10]
+            voltage = int(voltage_hex, 16) * 0.001
+            return voltage
+        return None
+
+    def get_heat_sink_temperature(self):
+        response = self.send_command('74001B0000')
+        if response and response.startswith('74001B'):
+            temperature_hex = response[6:10]
+            temperature = int(temperature_hex, 16) * 0.1
+            return temperature
+        return None
+
+    def get_optical_pulse_frequency(self):
+        response = self.send_command('7400710000')
+        if response and response.startswith('740071'):
+            frequency_hex = response[6:14]
+            frequency = int(frequency_hex, 16) * 0.001
+            return frequency
+        return None
+
+    def get_display_text(self):
+        response = self.send_command('7400780000')
+        if response and response.startswith('740078'):
+            text_hex = response[6:]
+            text = bytes.fromhex(text_hex).decode('utf-8')
+            return text
+        return None
+
+    def get_power_readout(self):
+        response = self.send_command('74007A0000')
+        if response and response.startswith('74007A'):
+            power = int(response[6:8], 16)
+            return power
+        return None
+
+    def set_emission(self, on=True):
+        emission_value = '01' if on else '00'
+        response = self.send_command(f'740030{emission_value}00')
+        return response and response.startswith('74')
+
+    def set_trig_mode(self, mode):
+        response = self.send_command(f'740031{mode:02X}00')
+        return response and response.startswith('74')
+
+    def set_interlock(self, interlock_value):
+        response = self.send_command(f'740032{interlock_value:04X}')
+        return response and response.startswith('74')
+
+    def get_status(self):
+        response = self.send_command('7400660000')
+        if response and response.startswith('740066'):
+            status_bits = response[6:]
+            return status_bits
+        return None
 
 def main():
-    laser_controller = SuperKCompactController(port='COM3')  
-    try:
-        laser_controller.turn_on() 
-        time.sleep(2)  
-        laser_controller.turn_off() 
-    finally:
-        laser_controller.close() 
+    laser = SuperKCompactControl()
+    laser.connect()
+    laser.disconnect()
 if __name__ == "__main__":
     main()
