a15c140e3c351634e8dd1f8a27eca7324c2d8e29
ahmad1596 <104497866+ahmad1596@users.noreply.github.com>
2024-01-25 17:26:47 +0000

    Update SpectrometerTimelapse_DataExtraction.py

--- SpectrometerTimelapse_DataExtraction.py
+++ SpectrometerTimelapse_DataExtraction.py
@@ -19,7 +19,35 @@ def plot_background_spectrum(filename):
     plt.title("Background Intensity vs. Wavelength", fontsize=16, fontweight="bold")
     plt.show()
 
-def plot_averaged_spectrum(filename):
+def plot_averaged_spectrum_without_fiber(filename):
+    with h5py.File(filename, "r") as file:
+        wavelengths = file["wavelengths"][:]
+        averaged_intensities = file["averaged_intensities"][:]
+    wavenumbers = 1 / (wavelengths * 1e-2)
+    fig_wavelengths, ax_wavelengths = plt.subplots()
+    fig_wavelengths.set_size_inches(8, 6)
+    fig_wavelengths.set_dpi(600)
+    ax_wavelengths.plot(wavelengths, averaged_intensities, label="Average Intensity (No Fiber)")
+    ax_wavelengths.set_xlabel("Wavelength (nm)", fontsize=14, fontweight="bold")
+    ax_wavelengths.set_ylabel("Average Intensity", fontsize=14, fontweight="bold")
+    ax_wavelengths.tick_params(axis="both", which="major", labelsize=12, direction="in")
+    ax_wavelengths.grid(color="gray", linestyle="--", linewidth=0.5)
+    ax_wavelengths.legend(loc="upper right", fontsize=8)
+    ax_wavelengths.set_title("Average Intensity (No Fiber) vs. Wavelength", fontsize=16, fontweight="bold")
+    fig_wavenumbers, ax_wavenumbers = plt.subplots()
+    fig_wavenumbers.set_size_inches(8, 6)
+    fig_wavenumbers.set_dpi(600)
+    ax_wavenumbers.plot(wavenumbers, averaged_intensities, label="Average Intensity (No Fiber)")
+    ax_wavenumbers.set_xlabel("Wavenumber (cm$^{-1}$)", fontsize=14, fontweight="bold")
+    ax_wavenumbers.set_ylabel("Average Intensity", fontsize=14, fontweight="bold")
+    ax_wavenumbers.tick_params(axis="both", which="major", labelsize=12, direction="in")
+    ax_wavenumbers.grid(color="gray", linestyle="--", linewidth=0.5)
+    ax_wavenumbers.legend(loc="upper right", fontsize=8)
+    ax_wavenumbers.set_title("Average Intensity (No Fiber) vs. Wavenumber", fontsize=16, fontweight="bold")
+    plt.show()
+
+
+def plot_averaged_spectrum_with_fiber(filename):
     with h5py.File(filename, "r") as file:
         wavelengths = file["wavelengths"][:]
         averaged_intensities = file["averaged_intensities"][:]
@@ -45,7 +73,40 @@ def plot_averaged_spectrum(filename):
     ax_wavenumbers.legend(loc="upper right", fontsize=8)
     ax_wavenumbers.set_title("Average Intensity vs. Wavenumber", fontsize=16, fontweight="bold")
     plt.show()
+    
+def plot_averaged_normalized_spectrum_fiber(filename):
+    with h5py.File(filename, "r") as file:
+        wavelengths = file["wavelengths"][:]
+        averaged_normalized_intensities = file["averaged_normalized_intensities"][:]
+    fig, ax = plt.subplots()
+    fig.set_size_inches(8, 6)
+    fig.set_dpi(600)
+    ax.plot(wavelengths, averaged_normalized_intensities, label="Averaged Normalized Intensity (With Fiber/Without Fiber)")
+    ax.set_xlabel("Wavelength (nm)", fontsize=14, fontweight="bold")
+    ax.set_ylabel("Averaged Normalized Intensity", fontsize=14, fontweight="bold")
+    ax.tick_params(axis="both", which="major", labelsize=12, direction="in")
+    ax.grid(color="gray", linestyle="--", linewidth=0.5)
+    ax.legend(loc="upper right", fontsize=8)
+    plt.title("Averaged Normalized Intensity vs. Wavelength (With Fiber)", fontsize=16, fontweight="bold")
+    plt.show()
       
+def plot_normalized_power_transmission(filename):
+    with h5py.File(filename, "r") as file:
+        wavelengths = file["wavelengths"][:]
+        normalized_spectral_power_transmission = file["normalized_spectral_power_transmission"][:]
+    fig, ax = plt.subplots()
+    fig.set_size_inches(8, 6)
+    fig.set_dpi(600)
+    ax.plot(wavelengths, normalized_spectral_power_transmission, label="Normalized Spectral Power Transmission")
+    ax.set_xlabel("Wavelength (nm)", fontsize=14, fontweight="bold")
+    ax.set_ylabel("Normalized Spectral Power Transmission", fontsize=14, fontweight="bold")
+    ax.tick_params(axis="both", which="major", labelsize=12, direction="in")
+    ax.grid(color="gray", linestyle="--", linewidth=0.5)
+    ax.legend(loc="upper right", fontsize=8)
+    plt.title("Normalized Spectral Power Transmission vs. Wavelength", fontsize=16, fontweight="bold")
+    plt.show()
+
+
 def plot_individual_spectra(filename):
     with h5py.File(filename, "r") as file:
         num_spectra = len(file.keys())
@@ -140,22 +201,31 @@ def main():
     try:
         filename_background = "./.h5 files/background_spectrum.h5"
         plot_background_spectrum(filename_background) 
+        
+        filename_averaged_spectra_without_fiber = "./.h5 files/2024-01-25_averaged_spectrum_without_fiber.h5"
+        plot_averaged_spectrum_without_fiber(filename_averaged_spectra_without_fiber)
        
-        filename_averaged_spectra = "./.h5 files/averaged_2023-09-04.h5"
-        plot_averaged_spectrum(filename_averaged_spectra)
+        filename_averaged_spectra_with_fiber = "./.h5 files/2024-01-25_averaged_spectrum_with_fiber.h5"
+        plot_averaged_spectrum_with_fiber(filename_averaged_spectra_with_fiber)
         
-        filename_individual_spectra = "./.h5 files/2023-09-04.h5"
-        plot_individual_spectra(filename_individual_spectra)
+        filename_averaged_normalized_spectrum_fiber = "./.h5 files/2024-01-25_averaged_normalized_spectrum_fiber.h5"
+        plot_averaged_normalized_spectrum_fiber(filename_averaged_normalized_spectrum_fiber)
         
-        filename_individual_spectra_timestamps = "./.h5 files/2023-09-04.h5"
-        target_wavelength = 633  
-        extract_and_plot_intensity_vs_time(filename_individual_spectra_timestamps, target_wavelength)
+        filename_normalized_power_transmission = "./.h5 files/2024-01-25_normalized_power_transmission.h5"
+        plot_normalized_power_transmission(filename_normalized_power_transmission)
+
+        filename_individual_spectra_with_fiber = "./.h5 files/2024-01-25_spectrum_with_fiber.h5"
+        plot_individual_spectra(filename_individual_spectra_with_fiber)
         
-        filename_individual_spectra_timestamps = "./.h5 files/2023-09-04.h5"
-        start_wavelength = 600
-        end_wavelength = 650   
-        num_points = 5
-        extract_and_plot_intensity_vs_time_range(filename_individual_spectra_timestamps, start_wavelength, end_wavelength, num_points)
+        filename_individual_spectra_timestamps_with_fiber = "./.h5 files/2024-01-25_spectrum_with_fiber.h5"
+        target_wavelength = 550
+        extract_and_plot_intensity_vs_time(filename_individual_spectra_timestamps_with_fiber, target_wavelength)
+        
+        filename_individual_spectra_timestamps_with_fiber = "./.h5 files/2024-01-25_spectrum_with_fiber.h5"
+        start_wavelength = 540
+        end_wavelength = 560 
+        num_points = 10
+        extract_and_plot_intensity_vs_time_range(filename_individual_spectra_timestamps_with_fiber, start_wavelength, end_wavelength, num_points)
         
     except Exception as e:
         print("An error occurred:", str(e))
