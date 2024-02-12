5bd35c44e171e899801df19c838232ecc37c9382
ahmad1596 <104497866+ahmad1596@users.noreply.github.com>
2024-02-12 00:35:50 +0000

    Update slits_dmd.py

--- slits_dmd.py
+++ slits_dmd.py
@@ -8,16 +8,17 @@ def create_output_folder(folder_name='mask_outputs'):
         os.makedirs(folder_name)
     return folder_name
 
-def save_output_files(file_name, binary_array, micromirror_pitch, slits_type, option, slit_locations=None, alternate_size=None, array_size=None, slit_width=None, slit_spacing=None, unit_size=None, radius=None, center_coordinates=None):
+def save_output_files(file_name, binary_array, micromirror_pitch, slits_type, option, slit_locations=None, alternate_size=None, array_size=None, slit_width=None, slit_spacing=None, unit_size=None, radius=None, center_coordinates=None, radius_inner=None, radius_outer=None):
     folder_name = create_output_folder()
-    slits_type_mapping = {1: 'horizontal', 2: 'vertical', 3: 'circular', 4: 'cross'}
+    slits_type_mapping = {1: 'horizontal', 2: 'vertical', 3: 'circular', 4: 'cross', 5: 'ring'}  # Update slits_type_mapping
     file_name_without_suffix = f'{slits_type_mapping[slits_type]}'
-    
     if slits_type == 3:
         file_name_without_suffix = 'circular'
         if radius is not None:
             file_name_without_suffix += f'_{radius}pixels_radius'
-    
+    elif slits_type == 5:  # Add this condition for the ring pattern
+        if radius_inner is not None and radius_outer is not None:
+            file_name_without_suffix = f'ring_radius_inner_{radius_inner}pixel_outer_{radius_outer}pixels'
     slit_positions_str = ''
     if option == 2:
         file_name_without_suffix = 'checkerboard'
@@ -38,11 +39,12 @@ def save_output_files(file_name, binary_array, micromirror_pitch, slits_type, op
                     file_name_without_suffix = f'vertical_slit_width_{slit_width}pixels_spacing_{slit_spacing}pixels'
     if unit_size is not None:
         file_name_without_suffix += f'_{unit_size}pixels'
-    
     if center_coordinates is not None:
         file_name_without_suffix += f'_({center_coordinates[0]},{center_coordinates[1]})'
-    
-    file_name_display = f"{file_name_without_suffix}_display"
+    if slits_type == 5:
+        file_name_display = f"{file_name_without_suffix}_radius_inner_{radius_inner}pixel_outer_{radius_outer}pixels_display"
+    else:
+        file_name_display = f"{file_name_without_suffix}_display"
     file_name_pixels = f"{file_name_without_suffix}"
     file_path_display = os.path.join(folder_name, f'{file_name_display}.png')
     file_path_pixels = os.path.join(folder_name, f'{file_name_pixels}.png')
@@ -52,7 +54,6 @@ def save_output_files(file_name, binary_array, micromirror_pitch, slits_type, op
     print(f"\nFiles saved in folder '{folder_name}':")
     print(f"1. {file_name_display}.png (size: {array_size[1]} x {array_size[0]})")
     print(f"2. {file_name_pixels}.png (size: {size_pixels[1]} x {size_pixels[0]})")
-
 
 def generate_slits(shape, slits_type, slit_coordinates, alternate_size=None):
     binary_array = np.zeros(shape, dtype=np.uint8)
@@ -149,7 +150,15 @@ def generate_cross_pattern(shape, intersection_point=None):
     print(f"Cross pattern created with intersection point at ({center_x},{center_y}).")
     return binary_array
 
-
+def generate_ring_pattern(shape, radius_inner, radius_outer):
+    binary_array = np.zeros(shape, dtype=np.uint8)
+    center_x, center_y = shape[1] // 2, shape[0] // 2
+    y_coordinates, x_coordinates = np.ogrid[:shape[0], :shape[1]]
+    mask_inner = (x_coordinates - center_x) ** 2 + (y_coordinates - center_y) ** 2 <= radius_inner ** 2
+    mask_outer = (x_coordinates - center_x) ** 2 + (y_coordinates - center_y) ** 2 <= radius_outer ** 2
+    mask_ring = np.logical_and(mask_outer, np.logical_not(mask_inner))
+    binary_array[mask_ring] = 255
+    return binary_array
 
 def plot_binary_array_display(file_path_display, binary_array, micromirror_pitch, slits_type, option, slit_coordinates=None, alternate_size=None):
     file_name = f'{slits_type}_'
@@ -194,16 +203,14 @@ def main():
     print("\n*DLP2000*")
     print(f"Display Resolution: {array_width} x {array_height}")
     print(f"Display Dimension: {array_width * micromirror_pitch} um x {array_height * micromirror_pitch} um")
-    
     while True:
         try:
-            configuration_type = int(input("\nMicromirror Array Configuration:\n1. Horizontal slits\n2. Vertical slits\n3. Circular pattern\n4. Cross pattern\nChoose configuration (1, 2, 3, or 4): "))
-            if configuration_type not in [1, 2, 3, 4]:
-                raise ValueError("Invalid input. Please enter 1 for 'Horizontal slits', 2 for 'Vertical slits', 3 for 'Circular pattern' or 4 for 'Cross pattern'.")
+            configuration_type = int(input("\nMicromirror Array Configuration:\n1. Horizontal slits\n2. Vertical slits\n3. Circular pattern\n4. Cross pattern\n5. Ring pattern\nChoose configuration (1, 2, 3, 4, or 5): "))
+            if configuration_type not in [1, 2, 3, 4, 5]:
+                raise ValueError("Invalid input. Please enter 1 for 'Horizontal slits', 2 for 'Vertical slits', 3 for 'Circular pattern', 4 for 'Cross pattern', or 5 for 'Ring pattern'.")
             break
         except ValueError as e:
             print(e)
-    
     slits_type = None
     slit_locations = None
     alternate_size = None
@@ -211,13 +218,11 @@ def main():
     slit_spacing = None
     unit_size = None
     radius = None
-    
     if configuration_type == 3:
         binary_array, radius = generate_circular_pattern((array_height, array_width))
         file_name = "circular_pattern"
         save_output_files(file_name, binary_array, micromirror_pitch, configuration_type, 0, slit_locations, alternate_size, (array_height, array_width), radius=radius)
         return
-    
     if configuration_type == 4:
         center_x = array_width // 2
         center_y = array_height // 2
@@ -231,13 +236,29 @@ def main():
                 return
         else:
             intersection_point = None
-    
         binary_array = generate_cross_pattern((array_height, array_width), intersection_point)
         file_name = "cross_pattern"
         save_output_files(file_name, binary_array, micromirror_pitch, configuration_type, 0, None, None, (array_height, array_width), radius=radius, center_coordinates=(center_x, center_y))
         return
-    
-    
+    if configuration_type == 5: 
+      print("Maximum ring diameter: 360 pixels (display height).")
+      while True:
+          try:
+              radius_inner_input = input("Enter the inner radius of the ring pattern in pixels: ")
+              radius_outer_input = input("Enter the outer radius of the ring pattern in pixels: ")
+              radius_inner = int(radius_inner_input)
+              radius_outer = int(radius_outer_input)
+              if 1 <= radius_inner < radius_outer:
+                  break
+              else:
+                  print("Invalid input. Please ensure that 1 <= inner radius < outer radius.")
+          except ValueError:
+              print("Invalid input. Please enter valid integers.")
+      binary_array = generate_ring_pattern((array_height, array_width), radius_inner, radius_outer)
+      file_name = f"ring_pattern_radius_{radius_inner}_{radius_outer}"
+      save_output_files(file_name, binary_array, micromirror_pitch, configuration_type, 0, None, None, (array_height, array_width),
+                          radius_inner=radius_inner, radius_outer=radius_outer)
+      return
     else:
         slits_type = configuration_type
 
@@ -251,8 +272,6 @@ def main():
             break
         else:
             print("Invalid option. Please choose 1 for 'Specific positions' or 2 for 'Alternate slits'.")
-
-
     if option == 1:
         while True:
             print("\nChoose an option:")
@@ -265,10 +284,8 @@ def main():
                 break
             else:
                 print("Invalid option. Please choose 1 for 'Single position', 2 for 'Multiple positions', or 3 for 'Range of positions'.")
-
         slit_input_info = "Info: single position / position_1, position_2, position_3,... / range: 'start' to 'end'"
         print(slit_input_info)
-
         while True:
             try:
                 slit_input = input(f"Enter slit location (allowed values: 0 to {array_height - 1}) for {'horizontal' if slits_type == 1 else 'vertical'} slits: ")
@@ -279,9 +296,7 @@ def main():
                     break
             except ValueError as e:
                 print(e)
-
         binary_array = generate_specific_positions((array_height, array_width), slits_type, slit_locations)
-
     elif option == 2:
         while True:
             print("\nChoose an option:")
@@ -293,7 +308,6 @@ def main():
                 break
             else:
                 print("Invalid option. Please choose 1 for 'Slits' or 2 for 'Checkerboard'.")
-
         if alternate_option == 1:
             orientation = 'vertical' if slits_type == 2 else 'horizontal'
             while True:
@@ -308,7 +322,6 @@ def main():
                     break
                 except ValueError as e:
                     print(e)
-
             while True:
                 try:
                     slit_spacing_input = input(f"Enter pixel slit spacing (allowed values: 1 to {allowed_range[1]}): ")
@@ -320,7 +333,6 @@ def main():
                     break
                 except ValueError as e:
                     print(e)
-
             binary_array = generate_alternate_slits((array_height, array_width), slits_type, slit_width, slit_spacing, orientation)
             file_name = f"{slits_type}_slits_width_{slit_width}pixel_spacing_{slit_spacing}pixel"
         elif alternate_option == 2:
@@ -335,17 +347,14 @@ def main():
                     break
                 except ValueError as e:
                     print(e)
-
             binary_array = generate_alternate_slits((array_height, array_width), slits_type, unit_size, unit_size, checkerboard=True)
             file_name = f"checkerboard_unit_{unit_size}"
         else:
             print("Invalid option. Choose 1 for 'Slits' or 2 for 'Checkerboard'.")
             return
-
     else:
         print("Invalid option. Choose 1 for 'Specific positions' or 2 for 'Alternate slits'.")
         return
-
     if binary_array is not None:
         if option == 1:
             if slit_locations is not None:
@@ -366,4 +375,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
