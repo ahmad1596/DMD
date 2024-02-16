13ef2991b8473a4719d5f6a3e5dc6bd4874958ef
ahmad1596 <104497866+ahmad1596@users.noreply.github.com>
2024-02-16 05:30:47 +0000

    Update camera_configure.py

--- camera_configure.py
+++ camera_configure.py
@@ -27,7 +27,6 @@ def get_frame_rate(hCam):
         return None
     return frame_rate.value
 
-
 def get_exposure_time(hCam):
     exposure_time = ueye.c_double()
     nRet = ueye.is_Exposure(hCam, ueye.IS_EXPOSURE_CMD_GET_EXPOSURE, exposure_time, ueye.sizeof(exposure_time))
@@ -75,20 +74,18 @@ def set_color_mode(hCam, m_nColorMode):
     if nRet != ueye.IS_SUCCESS:
         print("set_color_mode ERROR:", nRet)
 
-def set_aoi(hCam):
+def set_aoi(hCam, sInfo):
     rectAOI = ueye.IS_RECT()
     nRet = ueye.is_AOI(hCam, ueye.IS_AOI_IMAGE_GET_AOI, rectAOI, ueye.sizeof(rectAOI))
     if nRet != ueye.IS_SUCCESS:
         print("is_AOI ERROR")
         return None
-    rectAOI.s32Width //= 2
-    rectAOI.s32Height //= 2
-
+    rectAOI.s32Width = sInfo.nMaxWidth // 2
+    rectAOI.s32Height = sInfo.nMaxHeight // 2
     nRet = ueye.is_AOI(hCam, ueye.IS_AOI_IMAGE_SET_AOI, rectAOI, ueye.sizeof(rectAOI))
     if nRet != ueye.IS_SUCCESS:
         print("is_AOI ERROR")
         return None
-
     return rectAOI
 
 def allocate_image_memory(hCam, width, height, nBitsPerPixel):
@@ -119,10 +116,10 @@ def setup_camera():
     reset_camera_defaults(hCam)
     set_display_mode(hCam)
     m_nColorMode = ueye.INT()
-    nBitsPerPixel = ueye.INT(24)  # 3 channels and 8 bits per channel
+    nBitsPerPixel = ueye.INT(24)
     if int.from_bytes(sInfo.nColorMode.value, byteorder='big') == ueye.IS_COLORMODE_BAYER:
         ueye.is_GetColorDepth(hCam, nBitsPerPixel, m_nColorMode)
-    rectAOI = set_aoi(hCam)
+    rectAOI = set_aoi(hCam, sInfo)
     if rectAOI is None:
         ueye.is_ExitCamera(hCam)
         return None
@@ -147,81 +144,6 @@ def activate_camera_and_setup_image_memory(hCam, pcImageMemory, MemID, width, he
         print("is_InquireImageMem ERROR")
         return False
     return True
-
-def start_live_stream_and_save_frames(hCam, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch, bytes_per_pixel, target_frame_count=10):
-    if not activate_camera_and_setup_image_memory(hCam, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch):
-        return
-    elapsed_time = 0
-    frame_count = 0
-    frame_times = []
-    start_time = 0
-    save_frames = False
-    output_directory = r'C:\Users\DELL\Documents\optofluidics-master\optofluidics-master\Python\camera_outputs\\'
-    create_output_directory(output_directory)
-    while True:
-        array = ueye.get_data(pcImageMemory, width, height, nBitsPerPixel, pitch, copy=False)
-        frame = np.reshape(array, (height.value, width.value, bytes_per_pixel))
-        frame = cv2.resize(frame, (0, 0), fx=1.0, fy=1.0)
-        cv2.imshow("Python_uEye_OpenCV", frame)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
-        elif key == 13:
-            save_frames = True
-            start_time = time.perf_counter()
-        if save_frames and frame_count < target_frame_count:
-            frame_count += 1
-            elapsed_time = time.perf_counter() - start_time
-            frame_time = 1000 * elapsed_time / frame_count
-            frame_times.append(frame_time)
-            print(f"Frame {frame_count}: {frame_time:.2f} ms per frame")
-            frame_path = os.path.join(output_directory, f'captured_frame_{frame_count}.jpg')
-            cv2.imwrite(frame_path, frame)
-        if frame_count == target_frame_count:
-            save_frames = False
-    avg_frame_time = sum(frame_times) / len(frame_times)
-    print(f"\nTotal Number of Frames: {frame_count} frames")
-    print(f"Total Time Taken: {elapsed_time:.2f} seconds")
-    print(f"Average Frame Time: {avg_frame_time:.2f} ms per frame")
-    cv2.destroyAllWindows()
-    plot_frame_times(frame_times, frame_count, elapsed_time, avg_frame_time)
-
-def plot_frame_times(frame_times, frame_count, elapsed_time, avg_frame_time):
-    fig, ax = plt.subplots(figsize=(8, 6))
-    fig.set_dpi(600)
-    ax.set_xlabel("Number of Frames", fontsize=14, fontweight="bold")
-    ax.set_ylabel("Time Taken (milliseconds)", fontsize=14, fontweight="bold")
-    ax.set_title("Time Taken to Record Each Frame", fontsize=16, fontweight="bold")
-    ax.tick_params(axis="both", which="major", labelsize=12, direction="in")
-    ax.grid(color="gray", linestyle="--", linewidth=0.5)
-    mean_time = avg_frame_time
-    median_time = sorted(frame_times)[len(frame_times) // 2]
-    ax.errorbar(
-        range(1, len(frame_times) + 1),
-        frame_times,
-        fmt='o',
-        markersize=3,
-        capsize=3,
-        label='Time Taken',
-    )
-    ax.axhline(y=mean_time, color='blue', linestyle='-', label=f"Mean Time: {mean_time:.2f} ms")
-    ax.axhline(y=median_time, color='green', linestyle='-', label=f"Median Time: {median_time:.2f} ms")
-    ax.text(0.63, 0.80, f"Total Frames: {frame_count} frames", transform=ax.transAxes, fontsize=10, color='blue')
-    ax.text(0.63, 0.76, f"Total Time Taken: {elapsed_time:.2f} seconds", transform=ax.transAxes, fontsize=10, color='blue')
-    ax.legend(loc="lower right", fontsize=10)
-    plt.show()
-
-def capture_single_frame(hCam, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch, bytes_per_pixel, save_path):
-    nRet = ueye.is_FreezeVideo(hCam, ueye.IS_WAIT)
-    if nRet == ueye.IS_SUCCESS:
-        array = ueye.get_data(pcImageMemory, width, height, nBitsPerPixel, pitch, copy=False)
-        frame = np.reshape(array, (height.value, width.value, bytes_per_pixel))
-        frame = cv2.resize(frame, (0, 0), fx=1.0, fy=1.0)
-        cv2.destroyAllWindows()
-        cv2.imwrite(save_path, frame)
-    else:
-        print("is_FreezeVideo ERROR")
-    return save_path
 
 def set_exposure_time(hCam, exposure_time):
     nRet = ueye.is_Exposure(hCam, ueye.IS_EXPOSURE_CMD_SET_EXPOSURE, ueye.c_double(exposure_time), ueye.sizeof(ueye.c_double(exposure_time)))
@@ -260,24 +182,103 @@ def cleanup_camera(hCam, pcImageMemory, MemID):
     ueye.is_ExitCamera(hCam)
     cv2.destroyAllWindows()
 
+def start_live_stream_and_save_frames(hCam, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch, bytes_per_pixel, target_frame_count=10):
+    if not activate_camera_and_setup_image_memory(hCam, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch):
+        return
+    elapsed_time = 0
+    frame_count = 0
+    frame_times = []
+    start_time = 0
+    save_frames = False
+    output_directory = r'C:\Users\DELL\Documents\optofluidics-master\optofluidics-master\Python\camera_outputs\\'
+    create_output_directory(output_directory)
+    while True:
+        array = ueye.get_data(pcImageMemory, width, height, nBitsPerPixel, pitch, copy=False)
+        frame = np.reshape(array, (height.value, width.value, bytes_per_pixel))
+        desired_width = 640
+        desired_height = 512
+        resized_frame = cv2.resize(frame, (desired_width, desired_height))
+        new_frame = np.zeros((desired_height, desired_width, bytes_per_pixel), dtype=np.uint8)
+        start_x = (desired_width - resized_frame.shape[1]) // 2
+        start_y = (desired_height - resized_frame.shape[0]) // 2
+        new_frame[start_y:start_y + resized_frame.shape[0], start_x:start_x + resized_frame.shape[1], :] = resized_frame
+        cv2.imshow("Python_uEye_OpenCV", new_frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+        elif key == 13:
+            save_frames = True
+            start_time = time.perf_counter()
+        if save_frames and frame_count < target_frame_count:
+            frame_count += 1
+            elapsed_time = time.perf_counter() - start_time
+            frame_time = 1000 * elapsed_time / frame_count
+            frame_times.append(frame_time)
+            print(f"Frame {frame_count}: {frame_time:.2f} ms per frame")
+            frame_path = os.path.join(output_directory, f'captured_frame_{frame_count}.jpg')
+            cv2.imwrite(frame_path, new_frame)  # Save the new frame instead of the original
+        if frame_count == target_frame_count:
+            save_frames = False
+    avg_frame_time = sum(frame_times) / len(frame_times)
+    print(f"\nTotal Number of Frames: {frame_count} frames")
+    print(f"Total Time Taken: {elapsed_time:.2f} seconds")
+    print(f"Average Frame Time: {avg_frame_time:.2f} ms per frame")
+    cv2.destroyAllWindows()
+    plot_frame_times(frame_times, frame_count, elapsed_time, avg_frame_time)
+
+def plot_frame_times(frame_times, frame_count, elapsed_time, avg_frame_time):
+    fig, ax = plt.subplots(figsize=(8, 6))
+    fig.set_dpi(600)
+    ax.set_xlabel("Number of Frames", fontsize=14, fontweight="bold")
+    ax.set_ylabel("Time Taken (milliseconds)", fontsize=14, fontweight="bold")
+    ax.set_title("Time Taken to Record Each Frame", fontsize=16, fontweight="bold")
+    ax.tick_params(axis="both", which="major", labelsize=12, direction="in")
+    ax.grid(color="gray", linestyle="--", linewidth=0.5)
+    mean_time = avg_frame_time
+    median_time = sorted(frame_times)[len(frame_times)
+
+ // 2]
+    ax.errorbar(
+        range(1, len(frame_times) + 1),
+        frame_times,
+        fmt='o',
+        markersize=3,
+        capsize=3,
+        label='Time Taken',
+    )
+    ax.axhline(y=mean_time, color='blue', linestyle='-', label=f"Mean Time: {mean_time:.2f} ms")
+    ax.axhline(y=median_time, color='green', linestyle='-', label=f"Median Time: {median_time:.2f} ms")
+    ax.text(0.63, 0.80, f"Total Frames: {frame_count} frames", transform=ax.transAxes, fontsize=10, color='blue')
+    ax.text(0.63, 0.76, f"Total Time Taken: {elapsed_time:.2f} seconds", transform=ax.transAxes, fontsize=10, color='blue')
+    ax.legend(loc="lower right", fontsize=10)
+    plt.show()
+
+def capture_single_frame(hCam, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch, bytes_per_pixel, save_path):
+    nRet = ueye.is_FreezeVideo(hCam, ueye.IS_WAIT)
+    if nRet == ueye.IS_SUCCESS:
+        array = ueye.get_data(pcImageMemory, width, height, nBitsPerPixel, pitch, copy=False)
+        frame = np.reshape(array, (height.value, width.value, bytes_per_pixel))
+        frame = cv2.resize(frame, (0, 0), fx=1.0, fy=1.0)
+        cv2.destroyAllWindows()
+        cv2.imwrite(save_path, frame)
+    else:
+        print("is_FreezeVideo ERROR")
+    return save_path
+
 def main():
     hCam, width, height, nBitsPerPixel, pcImageMemory, MemID, bytes_per_pixel, pitch = setup_camera()
     set_camera_settings(hCam)
-
-    # Call the new functions to retrieve and print the values
     pixel_clock = get_pixel_clock(hCam)
     frame_rate = get_frame_rate(hCam)
     exposure_time = get_exposure_time(hCam)
     sInfo = get_sensor_info(hCam)
-    rectAOI = set_aoi(hCam)
+    rectAOI = set_aoi(hCam, sInfo)
     print_camera_settings_info(sInfo, rectAOI, width, height, nBitsPerPixel)
     output_directory = r'C:\Users\DELL\Documents\optofluidics-master\optofluidics-master\Python\camera_outputs\\'
     create_output_directory(output_directory)
-
     print("\nPress Enter to start saving 10 frames. Press q to quit.")
     start_live_stream_and_save_frames(hCam, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch, bytes_per_pixel, target_frame_count=10)
     cleanup_camera(hCam, pcImageMemory, MemID)
-    
     print(f"\nPixel Clock: {pixel_clock} MHz")
     print(f"Frame Rate: {frame_rate} fps")
     print(f"Exposure Time: {exposure_time:.6f} ms")
