3107d3be3f2a98b4dd146dec3319128fc85e68b3
ahmad1596 <104497866+ahmad1596@users.noreply.github.com>
2024-02-28 11:25:06 +0000

    Update guess_Nx_fdfd.py

--- guess_Nx_fdfd.py
+++ guess_Nx_fdfd.py
@@ -2,12 +2,11 @@ import numpy as np
 
 um = 1e-6  
 lam = 0.65 * um  
-lam_dx_ratio_target = 3
+lam_dx_ratio_target = 15
 dx = lam / lam_dx_ratio_target  
-Nx = int(np.ceil((2 * 26 * um) / dx)) + 1 
+Nx = int(np.ceil((2 * 30 * um) / dx)) + 1 
 
-x = np.linspace(-(26) * um, (26) * um, Nx) 
-
-print("lam/dx target: {:.2f}".format(lam_dx_ratio_target))
-print("Actual lam/dx: {:.2f}".format(lam/dx))
+print("lam/dx: {:.2f}".format(lam_dx_ratio_target))
+print("lam: {:.2f} nm".format(lam/1e-9))
+print("dx: {:.2f} nm".format(dx/1e-9))
 print("Nx: {}".format(Nx))
